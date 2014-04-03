d18ff4bc74d0650cb14789ca9c9c4efa58c406a2
Guilhem Doulcier <guilhem.doulcier@ens.fr>
2014-04-03 10:30:03 -0700

    genomes.py loading with hdm5 support.

--- genomes.py
+++ genomes.py
@@ -1,5 +1,12 @@
+import pandas
+from Bio import SeqIO
+import logging
+import os
+import options
+
 class Genomes(object):
-    """Data about reference genomes, object built from the refseq gbff file.
+    """
+    Data about reference genomes, object built from the refseq gbff file.
     """
     
     def __init__(self, fi ):
@@ -9,24 +16,125 @@ class Genomes(object):
         - `fi`:
         """
         self._fi = fi
-        self.names = None
-        self.prot2genome = None
-        self.taxonomy = None
-        self.prot2func = None
 
-    def load_geome(self,fi=None):
+
+
+        
+    def __len__(self):
+        return len(self.names)
+
+    def load_tara(self,h5_name = "tara.h5",fi_faa=None,fi_fna=None,fi_meta=None):
         """
-        Load the genomes names and associated proteins
-        Arguments:
-        - `self`:
-        - `fi`:
+        Load TARA metadata
+        """
+        store =  pandas.HDFStore(options.cache_folder+h5_name)
+
+
+        if not "metadata" in store:
+            # Metadata 
+            fi_meta = os.path.expanduser("~/data/tara/tara_station_metadata.csv") if fi_meta == None else os.path.expanduser(fi_meta)
+            meta = pandas.read_csv(fi_meta)
+            meta["Station"] = ["".join(k.split("_")) for k in meta["Station"]]
+            meta = meta.set_index("Station")
+            store.append('metadata',meta,format='table')
+            
+        # Contigs
+        if not "contigs" in store:
+            fi_fna = os.path.expanduser("~/data/tara/tara_contigs.txt") if fi_fna == None else os.path.expanduser(fi_fna) 
+            contigs = {"name":[],
+                      "Station":[]}
+
+            with open(fi_fna,"rb") as fna:
+                for l in fna:
+                    contigs["name"].append(l.rstrip())
+                    contigs["Station"].append(l.split("_")[0])
+
+            contigs = pandas.DataFrame(contigs).set_index("name")
+            store.append('contigs',contigs,format='table',data_columns=True)
+
+        if not "proteins" in store:
+            fi_faa = os.path.expanduser("~/data/tara/tara_prots.txt") if fi_faa == None else os.path.expanduser(fi_faa)
+            proteins = {"protein_id":[],
+                        "contig":[]}
+
+            with open(fi_faa,"rb") as faa:
+                for l in faa:
+
+                    proteins["protein_id"].append(l.split("#")[0].rstrip()[1:])
+                    proteins["contig"].append("_".join(l.split("_")[:2])[1:])
+
+            proteins = pandas.DataFrame(proteins).set_index("protein_id")
+            store.append('proteins',proteins,format='table',data_columns=True)
+        return store
+            
+    def load_refseq(self,fi=None,fi_taxa = None,h5_name="refseq.h5"):
+        """
+        INPUT :
+        - fi (str), a genebak file.
+        OUTPUT : 
+        - names (list) The genomes names (Index2genome)
+        - taxonomy (dict of list) map genome name on the list of taxonomic levels
+        - prot2genome (dict) map protein name to genome name
         """
 
-    def load_taxonomy(self,fi=None):
-        """
+        # Loading taxonomic classes 
+        fi = os.path.expanduser(options.data_folder+"refseq/phage.genomic.gbff") if not fi else fi 
+        fi_taxa = os.path.expanduser(options.data_folder+"ncbi_taxdump/refseqphage_taxa.pkl") if not fi_taxa else fi_taxa  
+
+
+        store =  pandas.HDFStore(options.cache_folder+h5_name)
+
+        if "genome" not in store or "proteins" not in store:
+            taxa = pandas.read_pickle(fi_taxa)
+            families = frozenset(taxa[taxa["Rank"]=="family"]["Name"])
+            genera = frozenset(taxa[taxa["Rank"]=="genus"]["Name"])
+
+
+            genome = {"name":[],
+                      "family":[],
+                      "genus":[],}
+
+            proteins = {"protein_id":[],
+                        "genome":[],
+                        "function":[],}
+
+            # Reading the gbff file thanks to BioPython
+            rec = SeqIO.parse(fi, "gb")
+            for r in rec:
+                name = r.id.split(".")[0]
+                genome["name"].append(name)
+
+                # Taxonomy 
+                genome["family"].append(None)
+                genome["genus"].append(None)
+                for t in r.annotations["taxonomy"]:
+                    if t in families:
+                        genome["family"][-1] = t
+                    elif t in genera:
+                        genome["genus"][-1]  = t
+                #Proteins 
+                for feature in r.features:
+                    if "protein_id" in feature.qualifiers:
+                        proteins["protein_id"].append(feature.qualifiers["protein_id"][0].split(".")[0])
+                        proteins["genome"].append(name)
+                        proteins["function"].append(feature.qualifiers["product"][0])
+
+
+            genome = pandas.DataFrame(genome).set_index("name")
+            proteins = pandas.DataFrame(proteins).set_index("protein_id")
+            store.append('genome',genome,format='table',data_columns=True)
+            store.append('proteins',proteins,format='table',data_columns=True)
+
+            nf = len(genome) - genome.family.count() 
+            ng = len(genome) - genome.genus.count()
+            if nf!=0 or ng!=0 :
+                logging.warning("{0} genomes without family and {1} genomes without genus.".format(nf,ng))
+
+        return store 
+
         
-        Arguments:
-        - `self`:
-        - `fi`:
-        """
-        
+
+    
+
+
+
--- options.py
+++ options.py
@@ -1,1 +1,4 @@
 """General options"""
+
+data_folder = "/home/guilhem/virus/data/"
+cache_folder = data_folder+"cache/"
