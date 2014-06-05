de96ddf05e2e811099c7334542c6b19189272637
Guilhem Doulcier <guilhem.doulcier@ens.fr>
2014-06-05 12:53:16 -0700

    license and readme

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='vcontact',
+      version='0.1dev',
+      description='Viral Contig Automatic Clutering and Taxonomy',
+      url='http://www.eleves.ens.fr/home/doulcier/projects/virus/',
+      author='Guilhem Doulcier',
+      long_description=open('README').read(),
+      author_email='guilhem.doulcier@ens.fr',
+      license='GPLv3',
+      packages=['vcontact'],)
